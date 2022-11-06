36d0a158187c37ecef0687b893cd69f69a963a4a
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2022-11-06 16:25:05 -0500

    solucion

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
-from tkinter import Tk, Button, Entry
+from tkinter import Tk, Button, Entry, END
 
 # Configuración ventana principal
 root = Tk()
-root.title(***)
+root.title("Calculadora POO")
 root.resizable(0,0)
-root.geometry(***)
+root.geometry("500x500")
+
+#funcionalidad
+def insertar(numero):
+    if len(pantalla.get()) == 0: 
+        pantalla.insert(0,numero)
+    else:
+        pantalla.insert(END,numero)
+
+def operar():
+    for simbolo in pantalla.get():
+        if ord(simbolo) > 45 and ord(simbolo) < 58: continue
+        else:  
+            partesOperacion = pantalla.get().split(simbolo)
+            pantalla.delete(0,END)
+            if simbolo == "*": 
+                pantalla.insert(0,str(float(partesOperacion[0])*float(partesOperacion[1])))
+            elif simbolo == "-":    
+                pantalla.insert(0,str(float(partesOperacion[0])-float(partesOperacion[1])))
+            elif simbolo == "+":    
+                pantalla.insert(0,str(float(partesOperacion[0])+float(partesOperacion[1])))
+            elif simbolo == "/":
+                pantalla.insert(0,str(float(partesOperacion[0])/float(partesOperacion[1])))
+
+
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
-pantalla.grid(row=0, column=0, columnspan=***, padx=1, pady=***)
+pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=1)
 
 # Configuración botones
-boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=1, pady=***)
-boton_2 = Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=***, pady=1)
-boton_3 = Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=***, pady=1)
-boton_4 = Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
-boton_5 = Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=1, padx=***, pady=***)
-boton_6 = Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=2, column=***, padx=***, pady=1)
-boton_7 = Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
-boton_8 = Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=1)
-boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=3, column=2, padx=***, pady=***)
-boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2").grid(row=4, column=***, columnspan=***, padx=1, pady=1)
-boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0).grid(row=***, column=***, padx=1, pady=1)
-boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=1, column=***, padx=***, pady=1)
-boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=3, padx=1, pady=1)
-boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=1)
-boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=***, column=***, padx=1, pady=***)
+boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(1)).grid(row=1, column=0, padx=1, pady=1)
+boton_2 = Button(root, text="2", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(2)).grid(row=1, column=1, padx=1, pady=1)
+boton_3 = Button(root, text="3", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(3)).grid(row=1, column=2, padx=1, pady=1)
+boton_4 = Button(root, text="4", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(5)).grid(row=2, column=0, padx=1, pady=1)
+boton_5 = Button(root, text="5", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(5)).grid(row=2, column=1, padx=1, pady=1)
+boton_6 = Button(root, text="6", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(6)).grid(row=2, column=2, padx=1, pady=1)
+boton_7 = Button(root, text="7", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(7)).grid(row=3, column=0, padx=1, pady=1)
+boton_8 = Button(root, text="8", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(8)).grid(row=3, column=1, padx=1, pady=1)
+boton_9 = Button(root, text="9", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: insertar(9)).grid(row=3, column=2, padx=1, pady=1)
+boton_igual = Button(root, text="=", width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2",command=operar).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
+boton_punto = Button(root, text=".", width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0,command=lambda: insertar(".")).grid(row=4, column=2, padx=1, pady=1)
+boton_mas = Button(root, text="+", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: insertar("+")).grid(row=1, column=3, padx=1, pady=1)
+boton_menos = Button(root, text="-", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: insertar("-")).grid(row=2, column=3, padx=1, pady=1)
+boton_multiplicacion = Button(root, text="*",  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: insertar("*")).grid(row=3, column=3, padx=1, pady=1)
+boton_division = Button(root, text="/", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: insertar("/")).grid(row=4, column=3, padx=1, pady=1)
+boton_limpiar = Button(root, text="C", width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2", command=lambda: pantalla.delete(0,END)).grid(row=5, column=0, columnspan=4, padx=1, pady=1)
 
 root.mainloop()
